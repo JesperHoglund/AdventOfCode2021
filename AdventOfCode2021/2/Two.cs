ed6c80617b23b8237f0975dabe6821ef72b38048
Jesper Höglund <jesper.hoglund@connectcompanies.se>
2021-12-02 08:26:53 +0100

    Day2

---
+++ AdventOfCode2021/2/Two.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021._2
+{
+    class Two
+    {
+        public string SolvePart1()
+        {
+            string[] actions = System.IO.File.ReadAllLines(@"C:\Users\Jesper\source\repos\AdventOfCode2021\AdventOfCode2021\2\Input.txt");
+            int depth = 0;
+            int horizontal = 0;
+
+            foreach (string action in actions)
+            {
+                switch (action.Split(" ")[0]) 
+                {
+                    case "forward":
+                        horizontal += int.Parse(action.Split(" ")[1]);
+                        break;
+                    case "down":
+                        depth += int.Parse(action.Split(" ")[1]);
+                        break;
+                    case "up":
+                        depth -= int.Parse(action.Split(" ")[1]);
+                        break;
+                }
+            }
+            return (horizontal*depth).ToString();
+        }
+        public string SolvePart2()
+        {
+            string[] actions = System.IO.File.ReadAllLines(@"C:\Users\Jesper\source\repos\AdventOfCode2021\AdventOfCode2021\2\Input.txt");
+            int depth = 0;
+            int horizontal = 0;
+            int aim = 0;
+
+            foreach (string action in actions)
+            {
+                switch (action.Split(" ")[0])
+                {
+                    case "forward":
+                        horizontal += int.Parse(action.Split(" ")[1]);
+                        depth += aim * int.Parse(action.Split(" ")[1]);
+                        break;
+                    case "down":
+                        aim += int.Parse(action.Split(" ")[1]);
+                        break;
+                    case "up":
+                        aim -= int.Parse(action.Split(" ")[1]);
+                        break;
+                }
+            }
+            return (horizontal * depth).ToString();
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -6,9 +6,13 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            _1.One one = new _1.One();
-            Console.WriteLine(one.SolvePart1v2());
-            Console.WriteLine(one.SolvePart2v2());
+            //_1.One one = new _1.One();
+            //Console.WriteLine(one.SolvePart1v2());
+            //Console.WriteLine(one.SolvePart2v2());
+
+            _2.Two two = new _2.Two();
+            Console.WriteLine(two.SolvePart1());
+            Console.WriteLine(two.SolvePart2());
         }
     }
 }
